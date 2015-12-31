1818e0a2a48be84e2c241b578f0728abf622a87a
Bryan Daniels <wormiii@hotmail.com>
2015-12-31 12:34:16 -0500

    knockout yay

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 "use strict";
 
-//x = 3;
-//window.alert("adsfd");
 
-function myViewModel() {
+function preMadeName(firstName, lastName) 
+{
+	var self = this;
+	self.fn = firstName;
+	self.ln = lastName;
+	self.fulln = function()
+	{
+		return self.fn + " " + self.ln;
+	}
+}
+
+function myViewModel()
+{
 	var self = this;
 
     self.firstName = ko.observable('Bryan');
@@ -20,8 +30,26 @@ function myViewModel() {
 
     self.listOfNames = ko.observableArray(
     	[
+    		new preMadeName("Barbara", "Daniels"),
+    		new preMadeName("Bonnie", "Fillenwarth"),
+    		new preMadeName("Jeff", "Siadek"),
+    		new preMadeName("Spot", "Oreo"),
+    		new preMadeName("Stuff", "Nonsense")
     	]
     );
+
+    self.selectedName = ko.observable(null);
+
+    self.setFromList = function()
+    {
+    	if (self.selectedName() == null)
+    	{
+    		return;
+    	}
+    	var sn = self.selectedName();
+    	self.firstName(sn.fn);
+    	self.lastName(sn.ln);
+    }
 
     self.fullName = ko.pureComputed(function() {
         return self.firstName() + " " + self.lastName();
@@ -31,4 +59,3 @@ function myViewModel() {
 ko.applyBindings(new myViewModel());
 
 
-//window.alert("end");
